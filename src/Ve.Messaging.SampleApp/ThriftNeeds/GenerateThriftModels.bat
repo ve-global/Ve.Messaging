2a93e330cd143bcdebf40dc1f29bb87b7b179e5c
Juanjo Guerrero <jcerezuela@veinteractive.com>
2016-06-13 16:46:44 +0100

    Adding Thrift Generation to the project

---
+++ src/Ve.Messaging.SampleApp/ThriftNeeds/GenerateThriftModels.bat
@@ -0,0 +1,3 @@
+..\..\packages\Thrift.0.9.1.3\tools\thrift-0.9.1.exe -gen csharp "Processed-Models-v1.thrift"
+
+pause
---
+++ src/Ve.Messaging.SampleApp/ThriftNeeds/Processed-Models-v1.thrift
@@ -0,0 +1,11 @@
+namespace csharp Ve.Messaging.Samples
+namespace java com.ve.messaging.samples
+
+struct HouseDto
+{
+	1: i32 Id,
+	2: string Name,
+	3: string Owner
+}
+
+
