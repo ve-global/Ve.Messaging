bd4a9e5bdb2b3cf4fcf9c399bc31dd8cf756ff74
Carlos Raffellini <carlos.raffellini@veinteractive.com>
2016-08-23 16:09:03 +0100

    Adding some test before refactor

---
+++ Ve.Messaging.Azure.ServiceBus.Test/MessagePublisherTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Ve.Messaging.Azure.ServiceBus.Publisher;
+using Ve.Messaging.Azure.ServiceBus.Publisher.Interfaces;
+using Ve.Messaging.Azure.ServiceBus.Publisher.Wrapper;
+using Ve.Messaging.Model;
+
+namespace Ve.Messaging.Azure.ServiceBus.Test
+{
+    public class MessagePublisherTest
+    {
+        [Test]
+        public async Task Should_send_messages_in_batch()
+        {
+            var resolverMock = new Mock<IPublisherClientResolver>();
+            var wrapperMock = new Mock<ITopicClientWrapper>();
+            resolverMock.Setup((_) => _.GetClient()).Returns(wrapperMock.Object);
+            var publisher = new MessagePublisher(resolverMock.Object);
+
+            await publisher.SendBatchAsync(new List<Message> { CreateEmptyMessage(), CreateEmptyMessage() });
+
+            wrapperMock.Verify((_) => _.SendAsync(It.IsAny<BrokeredMessage>()), Times.Exactly(2));
+        }
+
+        private Message CreateEmptyMessage()
+        {
+            return new Message(new Mock<Stream>().Object);
+        }
+    }
+}
